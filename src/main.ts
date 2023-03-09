e78df0332fc116fd3460c6ad881e9435ed5a7f64
ilpvc <2693285351@qq.com>
2023-03-09 09:22:16 +0800

    完成头部基本样式:lipstick:

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
+import naive from "naive-ui";
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(naive)
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+
+function _resolve(dir: string) {
+    return path.resolve(__dirname, dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': _resolve('src'),
+        }
+
+    }
 })
