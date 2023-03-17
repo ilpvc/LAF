91866ecde5ffccc7c905cb05b4744ff007a3724a
ilpvc <2693285351@qq.com>
2023-03-17 16:21:42 +0800

    消息页的用户信息:tada:

---
+++ src/components/Message.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="Message">
+    <div class="card-header">
+      <div class="header-detail">
+        <n-avatar
+            round
+            size="large"
+            src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+        />
+        <div class="header-detail-dd">
+          <h4>昵称</h4>
+          <p>{{reverseInformation}}</p>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+let reverseInformation = ref("")
+reverseInformation.value = "😆笑死了"
+
+</script>
+
+<style scoped lang="less">
+
+#Message {
+  display: flex;
+  align-items: center;
+  padding: 10px 10px;
+  border-bottom: 1px solid #8590a6;
+  width: 290px;
+  /*卡片头部*/
+  .card-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    /*卡片头部头像部分*/
+
+    .header-detail {
+      display: flex;
+      flex-direction: row;
+
+      /*头像部分的姓名和发布时间*/
+      .header-detail-dd {
+        display: flex;
+        flex-direction: column;
+        padding-left: 10px;
+
+      }
+
+      .header-detail-dd p {
+        display: block;
+        width: 150px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        color: #8590a6;
+      }
+    }
+
+  }
+}
+</style>
--- src/views/NoticeMessage/NoticeMessage.vue
+++ src/views/NoticeMessage/NoticeMessage.vue
@@ -1,13 +1,67 @@
 <template>
   <div id="NoticeMessage">
-    我是消息通知
+
+    <div class="left">
+      <div>
+        <h2>所有消息</h2>
+      </div>
+      <n-scrollbar>
+        <a href="#" v-for="i in 20" :key="i" class="l-item">
+          <Message></Message>
+        </a>
+      </n-scrollbar>
+
+
+    </div>
+
+    <div class="right">
+
+    </div>
+
   </div>
 </template>
 
 <script setup>
 
+import Message from "@/components/Message.vue";
 </script>
 
-<style scoped>
+<style scoped lang="less">
 
+#NoticeMessage {
+  display: flex;
+  width: 1000px;
+  padding: 10px;
+  background-color: white;
+  .left {
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    width: 290px;
+    border-right: 1px solid #121212;
+    height: 800px;
+    padding: 10px 0;
+
+
+
+  }
+
+  .right {
+    display: flex;
+    flex-direction: column;
+    width: 700px;
+    background-color: white;
+    padding: 20px 10px;
+  }
+
+}
+
+
+.l-item {
+  display: flex;
+  //background-color: #cccccc;
+  &:hover {
+    background-color: #cccccc;
+  }
+}
 </style>
